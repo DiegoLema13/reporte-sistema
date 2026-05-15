e0e2796f8a1ed09a01953bc3c4290b46954178cc
DiegoLema13 <dflema3@espe.edu.ec>
2026-05-15 01:27:21 -0500

    fix sendgrid attachment

--- app.py
+++ app.py
@@ -68,28 +68,41 @@ def enviar():
 def enviar_correo_async(archivo_pdf):
 
     def tarea():
-        import threading
-
-        message = Mail(
-            from_email="ekleain@gmail.com",
-            to_emails="ekleain@gmail.com",
-            subject="Nuevo reporte tecnico",
-            html_content="<strong>Adjunto reporte técnico</strong>"
-        )
-
-        with open(archivo_pdf, "rb") as f:
+        try:
+            import os
             import base64
-            encoded_file = base64.b64encode(f.read()).decode()
+            from sendgrid import SendGridAPIClient
+            from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
 
-        message.attachment = {
-            "content": encoded_file,
-            "type": "application/pdf",
-            "filename": archivo_pdf,
-            "disposition": "attachment"
-        }
-        sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
-        sg.send(message)
+            message = Mail(
+                from_email="TU_EMAIL_VERIFICADO@gmail.com",
+                to_emails="TU_EMAIL@gmail.com",
+                subject="Nuevo reporte tecnico",
+                html_content="Adjunto reporte técnico"
+            )
 
+            # leer pdf
+            with open(archivo_pdf, "rb") as f:
+                data = base64.b64encode(f.read()).decode()
+
+            attachment = Attachment(
+                FileContent(data),
+                FileName(archivo_pdf),
+                FileType("application/pdf"),
+                Disposition("attachment")
+            )
+
+            message.attachment = attachment
+
+            sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+            response = sg.send(message)
+
+            print("STATUS SENDGRID:", response.status_code)
+
+        except Exception as e:
+            print("ERROR SENDGRID:", str(e))
+
+    import threading
     threading.Thread(target=tarea).start()
 
 # =========================
