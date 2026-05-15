8d3284d5d729913f9b482390cfb03bcb49bf5352
DiegoLema13 <dflema3@espe.edu.ec>
2026-05-14 23:32:57 -0500

    sistema de reportes tecnicos tecnoplus con pdf

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import datetime
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+app = Flask(__name__)
+
+
+# =========================
+# PDF CON REPORTLAB
+# =========================
+def generar_pdf(nombre, provincia, problema, fecha, archivo_pdf):
+    c = canvas.Canvas(archivo_pdf, pagesize=letter)
+
+    c.setFont("Helvetica-Bold", 14)
+    c.drawString(180, 750, "REPORTE TECNICO")
+
+    c.setFont("Helvetica", 11)
+    c.drawString(100, 700, f"Nombre: {nombre}")
+    c.drawString(100, 680, f"Provincia: {provincia}")
+    c.drawString(100, 660, f"Problema: {problema}")
+    c.drawString(100, 640, f"Fecha: {fecha}")
+
+    c.save()
+
+
+# =========================
+# FORMULARIO
+# =========================
+@app.route("/")
+def home():
+    return render_template("formulario.html")
+
+
+# =========================
+# ENVIAR FORMULARIO
+# =========================
+@app.route("/enviar", methods=["POST"])
+def enviar():
+
+    nombre = request.form["nombre"]
+    provincia = request.form["provincia"]
+    problema = request.form["problema"]
+    fecha = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    # 1. GENERAR PDF
+    pdf_file = "reporte.pdf"
+    generar_pdf(nombre, provincia, problema, fecha, pdf_file)
+
+    # 2. ENVIAR CORREO
+    enviar_correo(pdf_file)
+
+    return "PDF generado y enviado correctamente"
+
+
+# =========================
+# CORREO
+# =========================
+def enviar_correo(archivo_pdf):
+
+    remitente = "ekleain@gmail.com"
+    password = "sbpl wmde xqat lhwy"
+    destino = "ekleain@gmail.com"
+
+    msg = MIMEMultipart()
+    msg["Subject"] = "Nuevo reporte tecnico"
+    msg["From"] = remitente
+    msg["To"] = destino
+
+    with open(archivo_pdf, "rb") as f:
+        parte = MIMEBase("application", "octet-stream")
+        parte.set_payload(f.read())
+
+    encoders.encode_base64(parte)
+    parte.add_header(
+        "Content-Disposition",
+        f"attachment; filename={archivo_pdf}"
+    )
+
+    msg.attach(parte)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(remitente, password)
+    server.send_message(msg)
+    server.quit()
+
+
+# =========================
+# RUN
+# =========================
+if __name__ == "__main__":
+    app.run(debug=True)
