e2e4d2379bbd7623f91a305f8fa828a1ab0e7454
DiegoLema13 <dflema3@espe.edu.ec>
2026-05-25 22:57:01 -0500

    se agrega cedula por default cuando se seleciona nombre del tecnico

---
+++ static/script.js
@@ -0,0 +1,87 @@
+const agencias = {
+
+    "Pichincha": [
+        "Quito Norte",
+        "Quito Sur",
+        "Sangolquí",
+        "Cayambe"
+    ],
+
+    "Guayas": [
+        "Guayaquil Centro",
+        "Durán",
+        "Milagro"
+    ]
+
+};
+
+
+const provinciaSelect =
+    document.getElementById("provincia");
+
+const agenciaSelect =
+    document.getElementById("agencia");
+
+
+provinciaSelect.addEventListener("change", function(){
+
+    const provincia =
+        this.value;
+
+    agenciaSelect.innerHTML = "";
+
+    const opcionInicial =
+        document.createElement("option");
+
+    opcionInicial.value = "";
+
+    opcionInicial.textContent =
+        "Seleccione una agencia";
+
+    agenciaSelect.appendChild(opcionInicial);
+
+    if(agencias[provincia]){
+
+        agencias[provincia].forEach(function(agencia){
+
+            const option =
+                document.createElement("option");
+
+            option.value = agencia;
+
+            option.textContent = agencia;
+
+            agenciaSelect.appendChild(option);
+
+        });
+
+    }
+
+});
+
+
+
+const tecnicos = {
+
+    "Diego Lema": "1312316811",
+    "Juan Perez": "9999999999"
+
+};
+
+
+const tecnicoSelect =
+    document.getElementById("tecnico");
+
+const cedulaInput =
+    document.getElementById("cedula");
+
+
+tecnicoSelect.addEventListener("change", function(){
+
+    const tecnico =
+        this.value;
+
+    cedulaInput.value =
+        tecnicos[tecnico] || "";
+
+});
